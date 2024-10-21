43f3d697f2744d2fbf02a5a9bb3359d0c8677348
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-10-21 10:15:23 -0300

    ajeitando css do menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,32 +4,32 @@ import '../style/Header.css';
 
 function Header() {
   return (
-    <header>
+    <div className='header'>
       <nav className="navbar">
-        <div className="navbar-brand">
-          <a href="#">Portfolio</a>
+        <div className="navbar-title">
+          <h1>Meu Portfolio</h1>
         </div>
-        <div className="navbar-items">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className="nav-link" href="#">Home</a>
+        <div className="navbar-menu">
+          <ul className="menu-items">
+            <li className="menu-item">
+              <a href="#home">Home</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Sobre mim</a>
+            <li className="menu-item">
+              <a href="#sobre">Sobre</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Experiência</a>
+            <li className="menu-item">
+              <a href="#experiencia">Experiência</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Competências</a>
+            <li className="menu-item">
+              <a href="#habilidades">Habilidades</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Contato</a>
+            <li className="menu-item">
+              <a href="#contato">Contato</a>
             </li>
           </ul>
         </div>
       </nav>
-    </header>
+    </div>
   );
 }
 
