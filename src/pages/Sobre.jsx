d80d26cde8353a1e901695b6e0dc62d904c4c9e1
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-10-21 13:37:56 -0300

    css sobre mim (falta foto

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,25 +1,33 @@
 
 import React from 'react';
 import Header from '../components/Header.jsx'
+import '../style/Sobre.css';
 
 function Sobre() {
     return (
-        <div className='sobre'>
+        <>
             <Header />
-            <div className='content-sobre'>
-            
-                Olá, me chamo Noemi Cho de Almeida!  
-                Tenho 21 anos e estou cursando Ciência da Computação
-                no Centro Federal de Educação Tecnológica Celso Suckow da Fonseca
-                (CEFET/RJ). Tenho grande interesse em Desenvolvimento Web e na 
-                área de Dados, com experiência prática em desenvolver e manter 
-                sistemas tanto no front-end quanto no back-end. Estou sempre 
-                buscando aprender e me atualizar, pois acredito que o aprendizado
-                contínuo é essencial para acompanhar as mudanças e evoluir como
-                desenvolvedora.
+            <div className='sobre'>
+                <div className='content-sobre'>
+                
+                    Olá, me chamo <h2>Noemi Cho de Almeida!</h2>
+                    <br></br>  
+                    Tenho 21 anos e estou cursando Ciência da Computação
+                    no Centro Federal de Educação Tecnológica Celso Suckow da Fonseca
+                    (CEFET/RJ).
+                    <br></br>  
+                    Tenho grande interesse em Desenvolvimento Web e na 
+                    área de Dados, com experiência prática em desenvolver e manter 
+                    sistemas tanto no front-end quanto no back-end. 
+                    <br></br>  
+                    Estou sempre 
+                    buscando aprender e me atualizar, pois acredito que o aprendizado
+                    contínuo é essencial para acompanhar as mudanças e evoluir como
+                    desenvolvedora.
 
+                </div>
             </div>
-        </div>
+        </>  
     );
 }
 
