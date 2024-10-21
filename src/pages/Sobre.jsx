730b3ece6c5e947158287298079ea2a43c11f4dd
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-10-21 13:23:13 -0300

    Sobre Mim (texto)

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -6,7 +6,19 @@ function Sobre() {
     return (
         <div className='sobre'>
             <Header />
-            Sobre
+            <div className='content-sobre'>
+            
+                Olá, me chamo Noemi Cho de Almeida!  
+                Tenho 21 anos e estou cursando Ciência da Computação
+                no Centro Federal de Educação Tecnológica Celso Suckow da Fonseca
+                (CEFET/RJ). Tenho grande interesse em Desenvolvimento Web e na 
+                área de Dados, com experiência prática em desenvolver e manter 
+                sistemas tanto no front-end quanto no back-end. Estou sempre 
+                buscando aprender e me atualizar, pois acredito que o aprendizado
+                contínuo é essencial para acompanhar as mudanças e evoluir como
+                desenvolvedora.
+
+            </div>
         </div>
     );
 }
