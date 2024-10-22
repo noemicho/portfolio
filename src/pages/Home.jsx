ac918aff0334a88354c3fd65419b3365fd946824
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-10-21 23:33:12 -0300

    fazendo download do meu curriculo no Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,17 @@
-
 import React from 'react';
-import Header from '../components/Header.jsx'
+import Header from '../components/Header.jsx';
 
 function Home() {
     return (
-        <div className='home'>
+        <>
             <Header />
-            <div id="carouselExample" class="carousel slide">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                    <img src="..." class="d-block w-100" alt="..."/>
-                    </div>
-                    <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="..."/>
-                    </div>
-                    <div class="carousel-item">
-                    <img src="..." class="d-block w-100" alt="..."/>
-                    </div>
-                </div>
-                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Previous</span>
-                </button>
-                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden">Next</span>
-                </button>
-                </div>
+            <div className="download-curriculo">
+                <h2>Baixe meu currículo</h2>
+                <a href="/files/CurrículoNoemiCho2024.pdf" download>
+                    <button className="download-button">Baixar Arquivo</button>
+                </a>
             </div>
+        </>
     );
 }
 
