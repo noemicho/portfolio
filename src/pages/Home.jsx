cec06d06100fe71e10a0e847d9268888f2f5e6a5
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-10-22 10:52:36 -0300

    finalizando css Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import '../style/Home.css'
-import flechaIcon from '../images/icon-flecha.png';
 
 function Home() {
     return (
-        <>    
-            <div className='introducao-home'>
-                <h2>Bem-vindo(a) ao meu Portfólio!</h2>
-                <p>Navegando pelo Menu, é possível visualizar páginas que fazem um resumo sobre mim, minhas experiências profissionais e competências. Além disso, em Contato, você pode anotar meus meios de contato, podendo visualizar meu Linkedin e Repositórios do Github.</p>
+        <>   
+            <div className='home'>
+                <div className='introducao-home'>
+                    <h2>Bem-vindo(a) ao meu Portfólio!</h2>
+                    <p>Navegando pelo Menu, é possível visualizar páginas que fazem um resumo sobre mim, minhas experiências profissionais e competências. Além disso, em Contato, você pode anotar meus meios de contato, podendo visualizar meu Linkedin e Repositórios do Github.</p>
 
-            </div>
-            <div className="download-curriculo">
-                <div className='titulo-download'>
-                    <h2>Baixe meu currículo</h2>
                 </div>
-                <div className='icon-button'>
-                    <img src={flechaIcon}></img>
-                    <a href="/files/CurrículoNoemiCho2024.pdf" download>
-                        <button className="download-button">Download</button>
-                    </a>
+                <div className="download-curriculo">
+                    <div className='titulo-download'>
+                        <h2>Baixe meu currículo</h2>
+                    </div>
+                    <div className='icon-button'>
+                        <a href="/files/CurrículoNoemiCho2024.pdf" download>
+                            <button className="download-button">Download</button>
+                        </a>
+                    </div>
                 </div>
             </div>
+            
         </>
     );
 }
