8bb2b609d728698c9855639b6d5b2bd1b19fdf89
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-10-22 12:19:11 -0300

    comçeando Competencias

--- src/pages/Competencias.jsx
+++ src/pages/Competencias.jsx
@@ -1,12 +1,46 @@
-
 import React from 'react';
+import '../style/Competencias.css';
 
 function Competencias() {
+    const habilidades = [
+        "Microsoft Office",
+        "HTML/CSS",
+        "Git/GitHub",
+        "JavaScript",
+        "SQL Server",
+        "MySQL",
+        "Edição de Fotos",
+        "Edição de Vídeo - Adobe After Effects",
+        "phpMyAdmin",
+        "PHP",
+        "React",
+        "NodeJs/Express",
+        "MongoDB",
+        "Java",
+        "Linguagem R",
+        "Python"
+    ];
+
     return (
-        <div className='competencias'>
-            
-            Competências
-        </div>
+        <>
+            <div className='competencias'>
+                <h3>Minhas Competências</h3>
+                <ul className='competencias-list'>
+                    {habilidades.map((habilidade, index) => (
+                        <li key={index}>{habilidade}</li>
+                    ))}
+                </ul>
+            </div>
+            <div className='linguagens'>
+                <h3>Linguagens</h3>
+                <ul>
+                    <li>Inglês Avançado</li>
+                    <li>Espanhol Intermediário</li>
+                    <li>Mandarim (Em progresso)</li>
+                </ul>
+            </div>
+        </>
+        
     );
 }
 
