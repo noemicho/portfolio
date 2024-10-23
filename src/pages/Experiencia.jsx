9296e3549d8c6f8aab08657035a05f30a8225d1c
Noemi Cho de Almeida <noemicho14@gmail.com>
2024-10-23 10:42:06 -0300

    css do Experiencias

--- src/pages/Experiencia.jsx
+++ src/pages/Experiencia.jsx
@@ -1,11 +1,49 @@
-
 import React from 'react';
+import '../style/Experiencia.css'; 
 
 function Experiencia() {
     return (
-        <div className='experiencia'>
+        <div className="timeline-container">
+            <h1>Experiência</h1>
             
-            Experiência
+            <div className="timeline">
+                
+                <div className="timeline-item">
+                    <div className="timeline-dot"></div>
+                    <div className="timeline-content">
+                        <h2>Estagiária TI</h2>
+                        <p className="empresa">Centauro Engenharia</p>
+                        <p className="descricao">
+                            Desenvolvo e realizo a manutenção do portal da empresa em PHP e JavaScript, também administrando e fazendo manutenção do banco de dados MySQL, por meio do phpMyAdmin. Além de realizar atividades de infraestrutura e suporte em TI.
+                        </p>
+                    </div>
+                </div>
+
+                
+                <div className="timeline-item">
+                    <div className="timeline-dot"></div>
+                    <div className="timeline-content">
+                        <h2>Jovem Aprendiz Relatórios</h2>
+                        <p className="empresa">Studio VS | Sistemas Exclusivos para a Gestão de Shopping Centers</p>
+                        <p className="descricao">
+                            Geração de relatórios atendendo às demandas específicas dos clientes. Utilizando habilidades avançadas em consultas SQL e produção de relatórios em diversos formatos, como PDF, Excel e CSV, utilizando ferramentas especializadas como o iReport e também em PHP.
+                        </p>
+                    </div>
+                </div>
+
+                
+                <div className="timeline-item">
+                    <div className="timeline-dot"></div>
+                    <div className="timeline-content">
+                        <h2>Jovem Aprendiz Infraestrutura TI</h2>
+                        <p className="empresa">Studio VS | CIEE Rio - Centro de Integração Empresa Escola</p>
+                        <p className="certificado">Certificado de Conclusão de Curso de Inglês - Wizard by Pearson</p>
+                        <p className="descricao">
+                            Montar, instalar, configurar e reconfigurar dispositivos de hardware; Auxiliar na manutenção de computadores, notebooks, monitores e impressoras; Gerenciar estoque do setor; Auxiliar usuários a partir de tickets-chamados.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
